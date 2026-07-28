d19b90193bdde9b7c56430b47952e1ebda12cbf6
tiesen243 <ttien56906@gmail.com>
2026-07-28 14:38:18 +0700

    feat(ci): setup release

--- tegami.ts
+++ tegami.ts
@@ -3,6 +3,16 @@ import { runCli } from 'tegami/cli'
 import { github } from 'tegami/plugins/github'
 
 const paper = tegami({
+  npm: {
+    client: 'bun',
+    updateLockFile: true,
+    onBreakPeerDep: 'set',
+    trustedPublish: {
+      provider: 'github',
+      workflow: 'release.yml',
+    },
+  },
+
   plugins: [
     github({
       repo: 'tiesen243/effect-tanstack-query',
